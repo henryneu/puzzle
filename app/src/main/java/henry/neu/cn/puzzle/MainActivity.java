2106b352a78e311d5b56ee3e89dd086974a768f4
henryneu <neuyimi@gmail.com>
2017-02-19 20:57:34 +0800

    搭建好选择拼图的界面.

--- app/src/main/java/henry/neu/cn/puzzle/MainActivity.java
+++ app/src/main/java/henry/neu/cn/puzzle/MainActivity.java
@@ -1,5 +1,7 @@
 package henry.neu.cn.puzzle;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
@@ -7,11 +9,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.GridView;
 import android.widget.PopupWindow;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import henry.neu.cn.puzzle.adapter.GridPicListAdapter;
 import henry.neu.cn.puzzle.utils.ScreenUtil;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Created by henryneu on 2017/2/19.
+     */
+
+    // GridView 显示图片
+    private GridView mGridViewPicList;
+    private List<Bitmap> mPicList;
+    // 主页图片资源ID
+    private int[] mResPicId;
 
     private PopupWindow mPopupWindow;
     private View mPopupView;
@@ -20,6 +37,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.puzzle_main);
+        mPicList = new ArrayList<Bitmap>();
+        mGridViewPicList = (GridView) findViewById(R.id.gridview_puzzle_main_pic_list);
+        // 初始化Bitmap数据
+        mResPicId = new int[]{R.drawable.pic1, R.drawable.pic2, R.drawable.pic3, R.drawable.pic4, R.drawable.pic5,
+                R.drawable.pic6, R.drawable.pic7, R.drawable.pic8, R.drawable.pic9, R.drawable.pic10,
+                R.drawable.pic11, R.drawable.pic12, R.drawable.pic13, R.drawable.pic14, R.drawable.pic15, R.mipmap.ic_launcher};
+        Bitmap[] bitmaps = new Bitmap[mResPicId.length];
+        for (int i = 0; i < bitmaps.length; i++) {
+            bitmaps[i] = BitmapFactory.decodeResource(getResources(), mResPicId[i]);
+            mPicList.add(bitmaps[i]);
+        }
+        mGridViewPicList.setAdapter(new GridPicListAdapter(MainActivity.this, mPicList));
     }
 
     /**
@@ -27,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
      *
      * @param view popup window
      */
+
     private void popupShow(View view) {
         int density = (int) ScreenUtil.getDeviceDensity(this);
         // 显示 popup window
--- app/src/main/java/henry/neu/cn/puzzle/activity/PuzzleMain.java
+++ app/src/main/java/henry/neu/cn/puzzle/activity/PuzzleMain.java
@@ -3,7 +3,7 @@ package henry.neu.cn.puzzle.activity;
 import android.app.Activity;
 
 /**
- * Created by user on 2017/2/19.
+ * Created by henryneu on 2017/2/19.
  */
 
 public class PuzzleMain extends Activity {
