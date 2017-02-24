dfd4122732d9f665743eceb5dbcbacb5dff893c7
henryneu <neuyimi@gmail.com>
2017-02-24 21:58:24 +0800

    搭建游戏主界面

---
+++ app/src/main/java/henry/neu/cn/puzzle/utils/ImagesUtil.java
@@ -0,0 +1,14 @@
+package henry.neu.cn.puzzle.utils;
+
+import henry.neu.cn.puzzle.bean.ItemBean;
+
+/**
+ * 图像工具类：实现图像的分割与自适应
+ *
+ * Created by henryneu on 2017/2/24.
+ */
+
+public class ImagesUtil {
+
+    public ItemBean itemBean;
+}
